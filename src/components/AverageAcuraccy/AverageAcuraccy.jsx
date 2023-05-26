77d1bfa5a84ca6a200f5de427ce716b7cf233519
Dênis Augusto Luft <denisluft8@gmail.com>
2023-05-26 15:02:22 -0300

    corrections on overall average accuracy chart, now fully functional

--- src/components/AverageAcuraccy/AverageAcuraccy.jsx
+++ src/components/AverageAcuraccy/AverageAcuraccy.jsx
@@ -1,57 +1,277 @@
 import { ContainerDiv } from "../index";
+import data from "../../data/data";
+import { PieChart, Pie, Cell } from "recharts";
+import {
+  AVDotLabel,
+  AVLabelContainer,
+  AVLabelText,
+  AVTitle,
+  InclinedDiv,
+} from "./AverageAcuraccy.styles";
 
-import { RadialBarChart, RadialBar, Legend } from "recharts";
+const dataA = [
+  { name: "90%", value: data.averageAcuraccy.blue1 },
+  { name: "Missing Value", value: 100 - data.averageAcuraccy.blue1 },
+];
+const COLORSA = ["#5BE1E3", "transparent"];
 
-const data = [
-  {
-    name: "30-34",
-    uv: 20,
-    pv: 1398,
-    fill: "#9CAE6E",
-  },
-  {
-    name: "25-29",
-    uv: 19,
-    pv: 4567,
-    fill: "#F17897",
-  },
-  {
-    name: "18-24",
-    uv: 90,
-    pv: 2400,
-    fill: "#5FE0E4",
-  },
+const dataB = [
+  { name: "19%", value: data.averageAcuraccy.pink1 },
+  { name: "Missing Value", value: 100 - data.averageAcuraccy.pink1 },
+];
+const COLORSB = ["#FD7691", "transparent"];
+
+const dataC = [
+  { name: "AM", value: 30 },
+  { name: "Missing Value", value: 70 },
+];
+const COLORSC = ["#C5E268", "transparent"];
+
+const dataD = [
+  { name: "90%", value: data.averageAcuraccy.blue2 },
+  { name: "Missing Value", value: 100 - data.averageAcuraccy.blue2 },
 ];
 
-const style = {
-  bottom: 0,
-  left: 0,
-  lineHeight: "24px",
-};
+const dataE = [
+  { name: "23%", value: data.averageAcuraccy.pink2 },
+  { name: "Missing Value", value: 100 - data.averageAcuraccy.pink2 },
+];
+
+const dataF = [
+  { name: "AM", value: 30 },
+  { name: "Missing Value", value: 70 },
+];
 
 export const AverageAcuraccy = () => {
   return (
-    <ContainerDiv>
-      <RadialBarChart
-        width={400}
-        height={300}
-        cx={150}
-        cy={150}
-        innerRadius={50}
-        outerRadius={100}
-        range={[100]}
-        barSize={20}
-        data={data}
-        label={(entry) => entry.name}
-      >
-        <RadialBar
-          minAngle={15}
-          label={{ position: "bottom", fill: "#fff" }}
-          clockWise
-          dataKey="uv"
-        />
-        <Legend iconSize={10} width={200} height={40} wrapperStyle={style} />
-      </RadialBarChart>
+    <ContainerDiv title="OVERALL AVERAGE ACCURACY">
+      <PieChart width={460} height={250}>
+        <Pie
+          data={dataA}
+          cx={100}
+          cy={130}
+          startAngle={90}
+          endAngle={-280}
+          innerRadius={45}
+          outerRadius={60}
+          fill="#8884d8"
+          paddingAngle={0}
+          dataKey="value"
+          label={false}
+        >
+          {dataA.map((entry, index) => (
+            <Cell
+              key={`cell-${index}`}
+              fill={COLORSA[index % COLORSA.length]}
+              stroke="transparent"
+            />
+          ))}
+        </Pie>
+        <Pie
+          data={dataB}
+          cx={100}
+          cy={130}
+          startAngle={90}
+          endAngle={-280}
+          innerRadius={34}
+          outerRadius={44}
+          fill="#8884d8"
+          paddingAngle={0}
+          dataKey="value"
+          label={false}
+        >
+          {dataB.map((entry, index) => (
+            <Cell
+              key={`cell-${index}`}
+              fill={COLORSB[index % COLORSB.length]}
+              stroke="transparent"
+            />
+          ))}
+        </Pie>
+        <Pie
+          data={dataC}
+          cx={100}
+          cy={130}
+          startAngle={150}
+          endAngle={-280}
+          innerRadius={23}
+          outerRadius={33}
+          fill="#8884d8"
+          paddingAngle={0}
+          dataKey="value"
+          label={false}
+        >
+          {dataC.map((entry, index) => (
+            <Cell
+              key={`cell-${index}`}
+              fill={COLORSC[index % COLORSC.length]}
+              stroke="transparent"
+            />
+          ))}
+        </Pie>
+        <Pie
+          data={dataD}
+          cx={350}
+          cy={130}
+          startAngle={90}
+          endAngle={-280}
+          innerRadius={45}
+          outerRadius={60}
+          fill="#8884d8"
+          paddingAngle={0}
+          dataKey="value"
+          label={false}
+        >
+          {dataD.map((entry, index) => (
+            <Cell
+              key={`cell-${index}`}
+              fill={COLORSA[index % COLORSA.length]}
+              stroke="transparent"
+            />
+          ))}
+        </Pie>
+        <Pie
+          data={dataE}
+          cx={350}
+          cy={130}
+          startAngle={90}
+          endAngle={-280}
+          innerRadius={34}
+          outerRadius={44}
+          fill="#8884d8"
+          paddingAngle={0}
+          dataKey="value"
+          label={false}
+        >
+          {dataE.map((entry, index) => (
+            <Cell
+              key={`cell-${index}`}
+              fill={COLORSB[index % COLORSB.length]}
+              stroke="transparent"
+            />
+          ))}
+        </Pie>
+        <Pie
+          data={dataF}
+          cx={350}
+          cy={130}
+          startAngle={150}
+          endAngle={-280}
+          innerRadius={23}
+          outerRadius={33}
+          fill="#8884d8"
+          paddingAngle={0}
+          dataKey="value"
+          label={false}
+        >
+          {dataF.map((entry, index) => (
+            <Cell
+              key={`cell-${index}`}
+              fill={COLORSC[index % COLORSC.length]}
+              stroke="transparent"
+            />
+          ))}
+        </Pie>
+      </PieChart>
+      <InclinedDiv
+        margintop="180px"
+        marginleft="-120px"
+        skewx="skewx(20deg)"
+        skewxb="skewx(-20deg)"
+        content={data.averageAcuraccy.blue1 + "%"}
+        diheight="20px"
+        borderleft
+        borderbottom
+      />
+      <InclinedDiv
+        margintop="70px"
+        marginleft="-120px"
+        content={data.averageAcuraccy.pink1 + "%"}
+        skewx="skewx(-20deg)"
+        skewxb="skewx(20deg)"
+        marginbottomlabel="40px"
+        diheight="40px"
+        bordertop
+        borderleft
+      />
+      <InclinedDiv
+        margintop="75px"
+        marginleft="-350px"
+        content="AM"
+        skewx="skewx(20deg)"
+        skewxb="skewx(-20deg)"
+        marginbottomlabel="40px"
+        diheight="40px"
+        borderright
+        bordertop
+      />
+      <InclinedDiv
+        margintop="75px"
+        marginleft="150px"
+        skewx="skewx(20deg)"
+        content="AM"
+        skewxb="skewx(-20deg)"
+        marginbottomlabel="40px"
+        diheight="40px"
+        borderright
+        bordertop
+      />
+      <InclinedDiv
+        margintop="70px"
+        marginleft="380px"
+        content={data.averageAcuraccy.pink2 + "%"}
+        skewx="skewx(-20deg)"
+        skewxb="skewx(20deg)"
+        marginbottomlabel="40px"
+        diheight="40px"
+        bordertop
+        borderleft
+      />
+      <InclinedDiv
+        margintop="180px"
+        marginleft="130px"
+        skewx="skewx(-20deg)"
+        skewxb="skewx(20deg)"
+        content={data.averageAcuraccy.blue2 + "%"}
+        diheight="20px"
+        borderright
+        borderbottom
+      />
+      <AVLabelContainer labelmargintop="230px" labelmarginleft="-240px">
+        <AVLabelText>
+          <AVDotLabel color="#C5E268"></AVDotLabel>
+          AM
+        </AVLabelText>
+        <AVLabelText>
+          <AVDotLabel color="#FD7691"></AVDotLabel>
+          1.20
+        </AVLabelText>
+        <AVLabelText>
+          <AVDotLabel color="#5BE1E3"></AVDotLabel>
+          29/32
+        </AVLabelText>
+      </AVLabelContainer>
+      <AVLabelContainer labelmargintop="230px" labelmarginleft="265px">
+        <AVLabelText>
+          <AVDotLabel color="#C5E268"></AVDotLabel>
+          AM
+        </AVLabelText>
+        <AVLabelText>
+          <AVDotLabel color="#FD7691"></AVDotLabel>
+          1.20
+        </AVLabelText>
+        <AVLabelText>
+          <AVDotLabel color="#5BE1E3"></AVDotLabel>
+          29/32
+        </AVLabelText>
+      </AVLabelContainer>
+
+      <AVTitle titlemargintop="10px" titlemarginleft="-240px">
+        LOGICAL
+      </AVTitle>
+      <AVTitle titlemargintop="10px" titlemarginleft="250px">
+        CREATIVE
+      </AVTitle>
     </ContainerDiv>
   );
 };
---
+++ src/components/AverageAcuraccy/AverageAcuraccy.styles.js
@@ -0,0 +1,62 @@
+import { styled } from "styled-components";
+
+export const InclinedDiv = styled.div`
+  width: 50px;
+  height: ${({ diheight }) => diheight};
+  background-color: transparent;
+  border-left: ${({ borderleft }) => (borderleft ? "1px solid #fff" : "")};
+  border-bottom: ${({ borderbottom }) =>
+    borderbottom ? "1px solid #fff" : ""};
+  border-top: ${({ bordertop }) => (bordertop ? "1px solid #fff" : "")};
+  border-right: ${({ borderright }) => (borderright ? "1px solid #fff" : "")};
+  position: absolute;
+  z-index: 999;
+  transform: ${({ skewx }) => skewx};
+  margin-top: ${({ margintop }) => margintop};
+  margin-left: ${({ marginleft }) => marginleft};
+
+  &::before,
+  &::after {
+    content: "${(props) => props.content}";
+    position: absolute;
+    margin-left: 6px;
+    margin-bottom: ${({ marginbottomlabel }) => marginbottomlabel || "0"};
+    transform: ${({ skewxb }) => skewxb};
+    color: white;
+    bottom: 0;
+    background-color: inherit;
+  }
+`;
+
+export const AVLabelContainer = styled.div`
+  position: absolute;
+  display: flex;
+  gap: 20px;
+  margin-top: ${({ labelmargintop }) => labelmargintop};
+  margin-left: ${({ labelmarginleft }) => labelmarginleft};
+`;
+
+export const AVDotLabel = styled.div`
+  height: 12px;
+  width: 12px;
+  border-radius: 50%;
+  background-color: ${({ color }) => color};
+`;
+
+export const AVLabelText = styled.span`
+  color: ${({ color }) => color || "#fff"};
+  font-size: 12px;
+  font-weight: ${({ labelweigth }) => labelweigth || "500"};
+  display: flex;
+  gap: 4px;
+  align-items: center;
+`;
+
+export const AVTitle = styled.span`
+  color: #fff;
+  position: absolute;
+  margin-top: ${({ titlemargintop }) => titlemargintop};
+  margin-left: ${({ titlemarginleft }) => titlemarginleft};
+  border-bottom: 2px solid #a87b2c;
+  padding: 0px 10px;
+`;
